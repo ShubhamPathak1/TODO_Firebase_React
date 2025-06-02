203e51c0d517ad0548a6b204135f5b2b45d40bf3
shubhampathak1 <pathakshubhu123@gmail.com>
2025-06-02 21:27:18 +0545

    updated fireconfig

--- src/config/firebaseExample.jsx
+++ src/config/firebaseExample.jsx
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore"
 
 // import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyDQlraaMkYt5YnPQ6hb9FKXmCxol55WcPs",
-  authDomain: "reactfirebase-1af5b.firebaseapp.com",
-  projectId: "reactfirebase-1af5b",
-  storageBucket: "reactfirebase-1af5b.firebasestorage.app",
-  messagingSenderId: "563053570796",
-  appId: "1:563053570796:web:4ea2f5eac8838d0fde53c9",
-  measurementId: "G-D31E7L5R8H"
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
